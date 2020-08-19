e1b9bd069794f404e476469aa11874f9465495c3
agarwal-123 <dhruv.agarwal31@yahoo.com>
2020-08-19 19:42:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainPage from './table'
+
+
+export default function IconLabelButtons() {
+
+  return (
+    <div>
+      <MainPage />
+    </div>
+  );
+}
---
+++ src/table.js
@@ -0,0 +1,130 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import Button from "@material-ui/core/Button";
+import Input from "@material-ui/core/TextField";
+
+
+
+export default function CustomPaginationActionsTable() {
+
+
+  function fetchData(n) {
+    console.log(n, "rf");
+    // var resp = await fetch("http://localhost:3000/");
+    // var ans = await resp.json();
+    fetch("http://localhost:3000/encode/?input="+n)
+      .then((response) => response.text())
+      .then((resp) => {
+        console.log(resp);
+        setEncode(resp)
+        // console.log(ans);
+        // var ans = resp;
+
+      });
+    // return resp;
+  }
+
+  function fetchData2(n) {
+    console.log(n, "rf");
+    // var resp = await fetch("http://localhost:3000/");
+    // var ans = await resp.json();
+    fetch("http://localhost:3000/decode/?input="+n)
+      .then((response) => response.text())
+      .then((resp) => {
+        console.log(resp);
+        setDecode(resp)
+        // console.log(ans);
+        // var ans = resp;
+
+      });
+    // return resp;
+  }
+
+
+  function submitEncodeHandler() {
+    var inputStr=document.getElementsByTagName('input')[0].value;
+    var flag=0;
+    for(var i=0;i<inputStr.length;i++) {
+      if(inputStr.charCodeAt(i)<97 || inputStr.charCodeAt(i)>122) flag=1;
+    }
+    if(inputStr.length>6 || flag) {
+      alert("Invalid Input!! String can have a maximum size of 6, and consist of small letter alphabets.");
+      return;
+    }
+    fetchData(inputStr);
+  }
+
+  function submitDecodeHandler() {
+    fetchData2(document.getElementsByTagName('input')[1].value);
+  }
+
+  const [encode, setEncode] = React.useState("");
+  const [decode, setDecode] = React.useState("");
+
+  return (
+    <div style={{ display:'flex',width:'80%',margin:'auto'}}>
+    <div
+      style={{
+        width: "50%",
+        margin: "auto",
+        minWidth: "400px",
+        marginTop: "50px",
+        
+      }}
+    >
+      <Input
+        id="standard-basic"
+        label="Encode here"
+        // type="number"
+        autoFocus='true'
+        InputLabelProps={{
+          shrink: true,
+        }}
+        style={{ marginBottom: "20px" }}
+      />
+
+      <Button
+        onClick={submitEncodeHandler}
+        variant="contained"
+        color="primary"
+        style={{ marginLeft: "10%" }}
+      >
+        Fetch
+      </Button>
+      <div>Encoded Value: {encode}</div>
+
+    </div>
+    <div
+    style={{
+      width: "50%",
+      margin: "auto",
+      minWidth: "500px",
+      marginTop: "50px",
+    }}
+  >
+    <Input
+      id="standard-basic"
+      label="Decode here"
+      // type="number"
+      autoFocus='true'
+      InputLabelProps={{
+        shrink: true,
+      }}
+      style={{ marginBottom: "20px" }}
+    />
+
+    <Button
+      onClick={submitDecodeHandler}
+      variant="contained"
+      color="primary"
+      style={{ marginLeft: "10%" }}
+    >
+      Fetch
+    </Button>
+    <div>Decoded Value: {decode}</div>
+
+  </div>
+  </div>
+  );
+}
